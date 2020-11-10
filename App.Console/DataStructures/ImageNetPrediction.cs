ed2138f0a7d3fe99ff36cadb870e18c91a8c25fd
La Quốc Thắng <quocthang0507@gmail.com>
2020-11-10 11:04:44 +0700

    Add new files

---
+++ App.Console/DataStructures/ImageNetPrediction.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.ML.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Console.DataStructures
+{
+    public class ImageNetPrediction
+    {
+        [ColumnName("grid")]
+        public float[] PredictedLabels;
+    }
+}
---
+++ App.Console/YoloParser/YoloBoundingBox.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Console.YoloParser
+{
+    public class YoloBoundingBox
+    {
+        public BoundingBoxDimensions Dimensions { get; set; }
+
+        public string Label { get; set; }
+
+        public float Confidence { get; set; }
+
+        public RectangleF Rect
+        {
+            get { return new RectangleF(Dimensions.X, Dimensions.Y, Dimensions.Width, Dimensions.Height); }
+        }
+
+        public Color BoxColor { get; set; }
+    }
+}
